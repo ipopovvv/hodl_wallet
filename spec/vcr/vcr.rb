3718b98d2e43f10c543ae6d8144ae06b3ce1066f
Ilya Popov <i.popovvv@outlook.com>
2025-05-04 21:56:54 +0300

    Init rspec + simplecov + vcr

---
+++ spec/vcr/vcr.rb
@@ -0,0 +1,10 @@
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :faraday
+  c.allow_http_connections_when_no_cassette = true
+
+  c.default_cassette_options = {
+    record: :once,
+    allow_unused_http_interactions: false
+  }
+end
